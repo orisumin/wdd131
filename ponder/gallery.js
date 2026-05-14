7b13ca53139f28c21a93b426e37b355e4321acfe
Garyeong Bae <orisumin@gmail.com>
2026-05-14 09:45:57 -0600

    finished responsive gallery

---
+++ ponder/gallery.js
@@ -0,0 +1,29 @@
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(anyevent) {
+    
+// Code to show modal  - Use event parameter 'e'   
+    console.log(anyevent.target.src);
+    modalImage.src = anyevent.target.src;
+    modalImage.src.replace('sm','full');
+    modal.showModal();
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+          
